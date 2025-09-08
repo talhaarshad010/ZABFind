a563f176381a93a0fc97a47005b727ac83fabc3b
SoftApps <122226517+talhaarshad010@users.noreply.github.com>
2025-09-09 04:42:49 +0500

    4:42

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -466,6 +466,177 @@
 
 // export default ChatScreen;
 
+// // src/screens/ChatScreen.js
+// import React, {useState, useEffect, useCallback} from 'react';
+// import {StyleSheet, View} from 'react-native';
+// import {GiftedChat, Message} from 'react-native-gifted-chat';
+// import io from 'socket.io-client';
+// import {useNavigation, useRoute} from '@react-navigation/native';
+// import Colors from '../styles/Colors';
+// import WrapperContainer from '../components/WrapperContainer';
+// import MyHeader from '../components/Header';
+// import Icon from 'react-native-vector-icons/Feather';
+// import axios from 'axios';
+// import {BaseUrl, SocketUrl} from '../config/Urls';
+// import {useSelector} from 'react-redux';
+
+// const socket = io(SocketUrl);
+
+// const ChatScreen = () => {
+//   const {user} = useSelector(state => state.Auth);
+//   console.log('user from selector in chat screen', user);
+//   const navigation = useNavigation();
+//   const route = useRoute();
+//   const {receiverId, receiverName} = route.params || {};
+//   console.log('data from route in chat screen', receiverId, receiverName);
+//   const [messages, setMessages] = useState([]);
+
+//   const canonicalRoomId = [user._id, receiverId].sort().join('-');
+//   console.log('canonicalRoomId in chatscreen:', canonicalRoomId);
+
+//   useEffect(() => {
+//     const fetchChatHistory = async () => {
+//       try {
+//         const response = await axios.get(
+//           `${BaseUrl}/student/chat/${receiverId}`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${user.token}`,
+//             },
+//           },
+//         );
+//         console.log('response of fetch chats:', response.data);
+//         const chatHistory = response.data.map(msg => ({
+//           _id: msg._id,
+//           text: msg.message,
+//           createdAt: new Date(msg.timestamp),
+//           user: {
+//             _id: (msg.senderId._id || msg.senderId).toString(),
+//             name:
+//               (msg.senderId._id || msg.senderId).toString() === user._id
+//                 ? user.fullName
+//                 : receiverName,
+//           },
+//         }));
+//         setMessages(chatHistory.reverse());
+//       } catch (error) {
+//         console.error('Error fetching chat history:', error);
+//       }
+//     };
+
+//     if (receiverId) {
+//       fetchChatHistory();
+//     }
+
+//     socket.emit('join_room', {roomId: canonicalRoomId, userId: user._id});
+
+//     socket.on('receive_message', messageData => {
+//       console.log('receive_message', messageData);
+//       if (messageData.senderId !== user._id) {
+//         setMessages(previousMessages => {
+//           if (previousMessages.some(msg => msg._id === messageData._id)) {
+//             return previousMessages;
+//           }
+//           return GiftedChat.append(previousMessages, [
+//             {
+//               _id: messageData._id,
+//               text: messageData.message,
+//               createdAt: new Date(messageData.timestamp),
+//               user: {
+//                 _id: messageData.senderId,
+//                 name: receiverName,
+//               },
+//             },
+//           ]);
+//         });
+//       }
+//     });
+
+//     return () => {
+//       socket.off('receive_message');
+//     };
+//   }, [
+//     canonicalRoomId,
+//     user._id,
+//     user.fullName,
+//     receiverName,
+//     receiverId,
+//     user.token,
+//   ]);
+
+//   const onSend = useCallback(
+//     (newMessages = []) => {
+//       const message = newMessages[0];
+//       socket.emit('send_message', {
+//         roomId: canonicalRoomId,
+//         message: message.text,
+//         senderId: user._id,
+//         receiverId,
+//       });
+//       setMessages(previousMessages =>
+//         GiftedChat.append(previousMessages, newMessages),
+//       );
+//     },
+//     [canonicalRoomId, user._id, receiverId],
+//   );
+
+//   // Custom renderMessage to avoid key prop warning
+//   const renderMessage = props => {
+//     const {currentMessage, ...messageProps} = props;
+//     return (
+//       <Message
+//         key={currentMessage._id} // Explicitly pass key
+//         {...messageProps}
+//         currentMessage={currentMessage}
+//       />
+//     );
+//   };
+
+//   return (
+//     <WrapperContainer>
+//       <MyHeader
+//         style={styles.header}
+//         ScreenName={`Chat with ${receiverName}`}
+//         leftView={<Icon name="arrow-left" size={24} color={Colors.black} />}
+//         onPressleft={() => navigation.goBack()}
+//       />
+//       <View style={styles.container}>
+//         <GiftedChat
+//           messages={messages}
+//           onSend={messages => onSend(messages)}
+//           user={{
+//             _id: user._id,
+//             name: user.fullName,
+//           }}
+//           placeholder="Type a message..."
+//           showUserAvatar={false}
+//           renderAvatar={null}
+//           renderMessage={renderMessage} // Add custom renderMessage
+//         />
+//       </View>
+//     </WrapperContainer>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   header: {
+//     backgroundColor: Colors.white,
+//     elevation: 10,
+//     shadowColor: '#000',
+//     shadowOffset: {width: 0, height: 2},
+//     shadowOpacity: 0.2,
+//     shadowRadius: 3,
+//     borderBottomWidth: 0.5,
+//     borderBottomColor: '#ccc',
+//     zIndex: 10,
+//   },
+// });
+
+// export default ChatScreen;
+
 // src/screens/ChatScreen.js
 import React, {useState, useEffect, useCallback} from 'react';
 import {StyleSheet, View} from 'react-native';
@@ -544,7 +715,7 @@ const ChatScreen = () => {
               createdAt: new Date(messageData.timestamp),
               user: {
                 _id: messageData.senderId,
-                name: receiverName,
+                name: messageData.senderName || receiverName,
               },
             },
           ]);
@@ -580,12 +751,11 @@ const ChatScreen = () => {
     [canonicalRoomId, user._id, receiverId],
   );
 
-  // Custom renderMessage to avoid key prop warning
   const renderMessage = props => {
     const {currentMessage, ...messageProps} = props;
     return (
       <Message
-        key={currentMessage._id} // Explicitly pass key
+        key={currentMessage._id}
         {...messageProps}
         currentMessage={currentMessage}
       />
@@ -611,7 +781,7 @@ const ChatScreen = () => {
           placeholder="Type a message..."
           showUserAvatar={false}
           renderAvatar={null}
-          renderMessage={renderMessage} // Add custom renderMessage
+          renderMessage={renderMessage}
         />
       </View>
     </WrapperContainer>
--- src/screens/Messages.js
+++ src/screens/Messages.js
@@ -334,10 +334,310 @@
 // });
 // export default Messages;
 // src/screens/Messages.js
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, FlatList, Pressable, View} from 'react-native';
+
+// import React, {useEffect, useState} from 'react';
+// import {StyleSheet, FlatList, Pressable, View} from 'react-native';
+// import {useSelector} from 'react-redux';
+// import axios from 'axios';
+// import MyText from '../components/textcomponent';
+// import Colors from '../styles/Colors';
+// import {
+//   responsiveFontSize,
+//   responsiveHeight,
+//   responsiveWidth,
+// } from 'react-native-responsive-dimensions';
+// import WrapperContainer from '../components/WrapperContainer';
+// import MyHeader from '../components/Header';
+// import Icon from 'react-native-vector-icons/Feather';
+// import {useNavigation} from '@react-navigation/native';
+// import {BaseUrl} from '../config/Urls';
+
+// const Messages = () => {
+//   const {user} = useSelector(state => state.Auth);
+//   console.log('user from selector in Messages screen', user);
+//   const navigation = useNavigation();
+//   const [chats, setChats] = useState([]);
+
+//   useEffect(() => {
+//     const fetchChats = async () => {
+//       try {
+//         const response = await axios.get(`${BaseUrl}/student/chats`, {
+//           headers: {
+//             Authorization: `Bearer ${user.token}`,
+//           },
+//         });
+//         console.log('all chat response:', response.data);
+//         setChats(response.data);
+//       } catch (error) {
+//         console.error('Error fetching chats:', error);
+//       }
+//     };
+
+//     fetchChats();
+//   }, [user._id, user.token]);
+
+//   const renderChat = ({item}) => (
+//     <Pressable
+//       style={styles.chatItem}
+//       onPress={() =>
+//         navigation.navigate('ChatScreen', {
+//           receiverId: item.receiverId,
+//           receiverName: item.receiverName,
+//         })
+//       }>
+//       <MyText
+//         text={item.receiverName}
+//         fontSize={responsiveFontSize(2)}
+//         fontWeight="bold"
+//         style={styles.chatName}
+//       />
+//       <MyText
+//         text={item.lastMessage}
+//         fontSize={responsiveFontSize(1.8)}
+//         style={styles.chatMessage}
+//       />
+//       <MyText
+//         text={new Date(item.timestamp).toLocaleTimeString()}
+//         fontSize={responsiveFontSize(1.5)}
+//         style={styles.chatTime}
+//       />
+//     </Pressable>
+//   );
+
+//   return (
+//     <WrapperContainer>
+//       <MyHeader
+//         style={styles.header}
+//         ScreenName="Messages"
+//         leftView={<Icon name="arrow-left" size={24} color={Colors.black} />}
+//         onPressleft={() => navigation.goBack()}
+//       />
+//       <View style={styles.container}>
+//         <FlatList
+//           data={chats}
+//           renderItem={renderChat}
+//           keyExtractor={item => item.roomId}
+//           ListEmptyComponent={
+//             <MyText
+//               text="No chats yet"
+//               fontSize={responsiveFontSize(2)}
+//               style={styles.emptyText}
+//             />
+//           }
+//         />
+//       </View>
+//     </WrapperContainer>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingHorizontal: responsiveWidth(3.5),
+//   },
+//   header: {
+//     backgroundColor: Colors.white,
+//     elevation: 10,
+//     shadowColor: '#000',
+//     shadowOffset: {width: 0, height: 2},
+//     shadowOpacity: 0.2,
+//     shadowRadius: 3,
+//     borderBottomWidth: 0.5,
+//     borderBottomColor: '#ccc',
+//     zIndex: 10,
+//   },
+//   chatItem: {
+//     padding: responsiveHeight(2),
+//     borderBottomWidth: 1,
+//     borderBottomColor: Colors.lightGray,
+//     marginBottom: responsiveHeight(1),
+//   },
+//   chatName: {
+//     color: Colors.black,
+//   },
+//   chatMessage: {
+//     color: Colors.darkGray,
+//   },
+//   chatTime: {
+//     color: Colors.grayInput,
+//     alignSelf: 'flex-end',
+//   },
+//   emptyText: {
+//     textAlign: 'center',
+//     marginTop: responsiveHeight(20),
+//     color: Colors.darkGray,
+//   },
+// });
+
+// export default Messages;
+
+// import React, {useEffect, useState, useCallback} from 'react';
+// import {
+//   StyleSheet,
+//   FlatList,
+//   Pressable,
+//   View,
+//   RefreshControl,
+// } from 'react-native';
+// import {useSelector} from 'react-redux';
+// import axios from 'axios';
+// import MyText from '../components/textcomponent';
+// import Colors from '../styles/Colors';
+// import {
+//   responsiveFontSize,
+//   responsiveHeight,
+//   responsiveWidth,
+// } from 'react-native-responsive-dimensions';
+// import WrapperContainer from '../components/WrapperContainer';
+// import MyHeader from '../components/Header';
+// import Icon from 'react-native-vector-icons/Feather';
+// import {useNavigation} from '@react-navigation/native';
+// import {BaseUrl} from '../config/Urls';
+
+// const Messages = () => {
+//   const {user} = useSelector(state => state.Auth);
+//   const navigation = useNavigation();
+
+//   const [chats, setChats] = useState([]);
+//   const [refreshing, setRefreshing] = useState(false);
+
+//   // fetch chats function
+//   const fetchChats = useCallback(async () => {
+//     try {
+//       const response = await axios.get(`${BaseUrl}/student/chats`, {
+//         headers: {
+//           Authorization: `Bearer ${user.token}`,
+//         },
+//       });
+//       console.log('all chat response:', response.data);
+//       setChats(response.data);
+//     } catch (error) {
+//       console.error('Error fetching chats:', error);
+//     }
+//   }, [user.token]);
+
+//   // initial load
+//   useEffect(() => {
+//     fetchChats();
+//   }, [fetchChats]);
+
+//   // pull to refresh handler
+//   const onRefresh = async () => {
+//     setRefreshing(true);
+//     await fetchChats();
+//     setRefreshing(false);
+//   };
+
+//   const renderChat = ({item}) => (
+//     <Pressable
+//       style={styles.chatItem}
+//       onPress={() =>
+//         navigation.navigate('ChatScreen', {
+//           receiverId: item.receiverId,
+//           receiverName: item.receiverName,
+//         })
+//       }>
+//       <MyText
+//         text={item.receiverName}
+//         fontSize={responsiveFontSize(2)}
+//         fontWeight="bold"
+//         style={styles.chatName}
+//       />
+//       <MyText
+//         text={item.lastMessage}
+//         fontSize={responsiveFontSize(1.8)}
+//         style={styles.chatMessage}
+//       />
+//       <MyText
+//         text={new Date(item.timestamp).toLocaleTimeString()}
+//         fontSize={responsiveFontSize(1.5)}
+//         style={styles.chatTime}
+//       />
+//     </Pressable>
+//   );
+
+//   return (
+//     <WrapperContainer>
+//       <MyHeader
+//         style={styles.header}
+//         ScreenName="Messages"
+//         leftView={<Icon name="arrow-left" size={24} color={Colors.black} />}
+//         onPressleft={() => navigation.goBack()}
+//       />
+//       <View style={styles.container}>
+//         <FlatList
+//           data={chats}
+//           renderItem={renderChat}
+//           keyExtractor={item => item.roomId}
+//           refreshControl={
+//             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+//           }
+//           ListEmptyComponent={
+//             <MyText
+//               text="No chats yet"
+//               fontSize={responsiveFontSize(2)}
+//               style={styles.emptyText}
+//             />
+//           }
+//         />
+//       </View>
+//     </WrapperContainer>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingHorizontal: responsiveWidth(3.5),
+//   },
+//   header: {
+//     backgroundColor: Colors.white,
+//     elevation: 10,
+//     shadowColor: '#000',
+//     shadowOffset: {width: 0, height: 2},
+//     shadowOpacity: 0.2,
+//     shadowRadius: 3,
+//     borderBottomWidth: 0.5,
+//     borderBottomColor: '#ccc',
+//     zIndex: 10,
+//   },
+//   chatItem: {
+//     padding: responsiveHeight(2),
+//     borderBottomWidth: 1,
+//     borderBottomColor: Colors.lightGray,
+//     marginBottom: responsiveHeight(1),
+//   },
+//   chatName: {
+//     color: Colors.black,
+//   },
+//   chatMessage: {
+//     color: Colors.darkGray,
+//   },
+//   chatTime: {
+//     color: Colors.grayInput,
+//     alignSelf: 'flex-end',
+//   },
+//   emptyText: {
+//     textAlign: 'center',
+//     marginTop: responsiveHeight(20),
+//     color: Colors.darkGray,
+//   },
+// });
+
+// export default Messages;
+
+import React, {useEffect, useState, useCallback} from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  Pressable,
+  View,
+  RefreshControl,
+  ActivityIndicator,
+} from 'react-native';
 import {useSelector} from 'react-redux';
 import axios from 'axios';
+import io from 'socket.io-client';
 import MyText from '../components/textcomponent';
 import Colors from '../styles/Colors';
 import {
@@ -349,31 +649,99 @@ import WrapperContainer from '../components/WrapperContainer';
 import MyHeader from '../components/Header';
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
-import {BaseUrl} from '../config/Urls';
+import {BaseUrl, SocketUrl} from '../config/Urls';
+
+const socket = io(SocketUrl);
 
 const Messages = () => {
   const {user} = useSelector(state => state.Auth);
   console.log('user from selector in Messages screen', user);
   const navigation = useNavigation();
   const [chats, setChats] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
+  // Fetch chats function
+  const fetchChats = useCallback(async () => {
+    try {
+      setRefreshing(true);
+      const response = await axios.get(`${BaseUrl}/student/chats`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log('all chat response:', response.data);
+      setChats(
+        response.data.sort(
+          (a, b) => new Date(b.timestamp) - new Date(a.timestamp),
+        ),
+      );
+    } catch (error) {
+      console.error('Error fetching chats:', error);
+    } finally {
+      setRefreshing(false);
+    }
+  }, [user.token]);
+
+  // Initial load and Socket.IO setup
   useEffect(() => {
-    const fetchChats = async () => {
-      try {
-        const response = await axios.get(`${BaseUrl}/student/chats`, {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        console.log('all chat response:', response.data);
-        setChats(response.data);
-      } catch (error) {
-        console.error('Error fetching chats:', error);
-      }
-    };
-
     fetchChats();
-  }, [user._id, user.token]);
+
+    // Join all relevant chat rooms for the user
+    socket.emit('join_user_chats', {userId: user._id});
+
+    // Listen for new messages
+    socket.on('receive_message', messageData => {
+      console.log('receive_message in Messages:', messageData);
+      const {
+        roomId,
+        senderId,
+        receiverId,
+        message,
+        timestamp,
+        senderName,
+        receiverName,
+      } = messageData;
+      const otherUserId = senderId === user._id ? receiverId : senderId;
+      const otherUserName = senderId === user._id ? receiverName : senderName;
+
+      setChats(prevChats => {
+        const existingChatIndex = prevChats.findIndex(
+          chat => chat.roomId === roomId,
+        );
+        const updatedChat = {
+          roomId,
+          receiverId: otherUserId,
+          receiverName: otherUserName || 'Unknown',
+          lastMessage: message,
+          timestamp,
+        };
+
+        if (existingChatIndex >= 0) {
+          const updatedChats = [...prevChats];
+          updatedChats[existingChatIndex] = updatedChat;
+          return updatedChats.sort(
+            (a, b) => new Date(b.timestamp) - new Date(a.timestamp),
+          );
+        } else {
+          return [updatedChat, ...prevChats].sort(
+            (a, b) => new Date(b.timestamp) - new Date(a.timestamp),
+          );
+        }
+      });
+    });
+
+    // Cleanup on unmount
+    return () => {
+      socket.off('receive_message');
+    };
+  }, [user._id, fetchChats]);
+
+  // Pull-to-refresh handler
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchChats();
+    setRefreshing(false);
+  }, [fetchChats]);
 
   const renderChat = ({item}) => (
     <Pressable
@@ -416,6 +784,14 @@ const Messages = () => {
           data={chats}
           renderItem={renderChat}
           keyExtractor={item => item.roomId}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors={[Colors.primary]}
+              progressBackgroundColor={Colors.white}
+            />
+          }
           ListEmptyComponent={
             <MyText
               text="No chats yet"
